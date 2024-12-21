eb81cdca398da16ab7460f2e82ddf5c42fd1384e
hk416hasu <hk416hasu@126.com>
2024-12-21 09:36:15 +0800

    add POST address, then realize the interacion from front-end to back-end

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 const { startServer } = require('./module_self/server');
 startServer(__dirname);
-
-// const child_process = require('child_process');
-// child_process.exec("./exe/hello yixuan liguan tianci guoli", (err, stdout, stderr) => {
-//     if (err) {
-//         //
-//     } else {
-//         console.log(stdout);
-//     }
-// });
--- module_self/server.js
+++ module_self/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { execFile }  = require('child_process');
 const path = require('path');
 const app = express();
 
@@ -10,6 +11,25 @@ function startServer(rootPath) {
     });
 
     app.use(express.static(path.join(rootPath, 'public')));
+
+    app.post('/executeHello', (req, res) => {
+        //const elfPath = '/home/ubuntu/nodejs/exe/hello';
+        const elfPath = './exe/hello';
+        const args = ['yixuan', 'liguan', 'tianci', 'guoli'];
+
+        execFile(elfPath, args, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`执行错误: ${error.message}`);
+              return res.status(500).send('执行错误');
+            }
+            if (stderr) {
+              console.error(`标准错误: ${stderr}`);
+              return res.status(500).send(`标准错误: ${stderr}`);
+            }
+            res.send(stdout.trim()); // 返回 ELF 文件的输出
+            console.log(stdout.trim());
+        });
+    });
 
     app.use((req, res, next) => {
         res.status(404).sendFile(path.join(rootPath, 'public', '404.html'));
